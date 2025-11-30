f8ef2ab6888b605fcddd9c74db38b6ba556671ca
ClementSellyPro <clementselly.pro@gmail.com>
2025-11-30 15:32:17 +0400

    Transferer la partie user-button de HeaderComponent, dans un UserButtonComponent, et ajouter un menu qui permet a l'utilisateur de selectionner entre Creer un compte et Connexion

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import {UserButtonComponent} from './user-button/user-button.component';
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [UserButtonComponent],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
---
+++ src/app/components/header/user-button/user-button.component.ts
@@ -0,0 +1,29 @@
+import {Component, ElementRef, HostListener, ViewChild} from '@angular/core';
+import {NgIf} from '@angular/common';
+
+@Component({
+  selector: 'app-user-button',
+  imports: [
+    NgIf
+  ],
+  templateUrl: './user-button.component.html',
+  styleUrl: './user-button.component.css'
+})
+export class UserButtonComponent {
+  isMenuOpen = true;
+
+  @ViewChild('container') container!: ElementRef;
+
+  toggleMenu() {
+    this.isMenuOpen = !this.isMenuOpen;
+  }
+
+  // Referme le menu si l'utilisateur clique en dehors
+  @HostListener('document:click', ['$event.target'])
+  onClickOutside(targetElement: HTMLElement) {
+    const clickedInside = this.container.nativeElement.contains(targetElement);
+    if (!clickedInside) {
+      this.isMenuOpen = false;
+    }
+  }
+}
