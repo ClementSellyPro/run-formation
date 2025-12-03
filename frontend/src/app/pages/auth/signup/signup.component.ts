598b03aa0662e694188a45b3d6ac9e51c09e8de2
ClementSellyPro <clementselly.pro@gmail.com>
2025-12-03 13:52:27 +0400

    Réarranger la page Signup et le mettre en tant que page 'index' pour demander à l'utilisateur de créer un compte ou se connecter

--- frontend/src/app/pages/auth/signup/signup.component.ts
+++ frontend/src/app/pages/auth/signup/signup.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
+import {RouterLink} from '@angular/router';
 
 @Component({
   selector: 'app-signup',
-  imports: [],
+  imports: [
+    RouterLink
+  ],
   templateUrl: './signup.component.html',
-  styleUrl: '../auth.css'
+  styleUrls: ['../auth.css', 'signup.component.css']
 })
 export class SignupComponent {
 
